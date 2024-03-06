a7abbaf14cbcf934dabffc4002a4786ac404d392
Eliza266 <eliza.perez266@gmail.com>
2024-03-06 11:16:40 -0500

    segundo

--- ejercicio1/converMoneda.py
+++ ejercicio1/converMoneda.py
@@ -3,7 +3,7 @@
 # 1 yen = 26.30 pesos
 # 1 dólar = 3944 pesos
 # 1 euro = 4279 pesos
-import os
+import os,sys
 #funcion menu
 def menu():
     listaMenu=['1','2','3','4','5']
@@ -20,10 +20,12 @@ def menu():
         else: 
             print('Opcion Incorrecta')
             os.system('pause')
-            os.system('pause')
+            os.system('cls')
             menu()
-    except ValueError:
-        print('Opcion Incorrecta')
+    except KeyboardInterrupt:
+        print('ERROR')
+        os.system('pause')
+        os.system('cls')
         menu()
     
 
@@ -33,21 +35,42 @@ def menu():
     opt=input('Ingrese la opcion que desea realizar: /n1.Pesos a dolares: /n2. ' )
 # funcionn principal
 if __name__=='__main__':
-    
-    opt=menu()
     dolar=3944
     euro = 4279
     yen = 26.30
+    varBool=True
+    while varBool:
+        opt=menu()
+        if opt=='1':
+            pesos=float(input('Ingrese la cantidad de pesos que desea convertir: '))
+            pesosADolar= pesos/dolar
+            print(f'La cantidad de {pesos} a dolar es de: {pesosADolar}')
+            os.system('pause')
+            os.system('cls')
+        elif opt=='2':
+            pesos=float(input('Ingrese la cantidad de pesos que desea convertir: '))
+            pesosAEuro=pesos/euro
+            print(f'La cantidad de {pesos} a Euros es de: {pesosAEuro}')
+            os.system('pause')
+            os.system('cls')
+        elif opt=='3':
+            Euro=float(input('Ingrese la cantidad de Euros que desea convertir: '))
+            euroAPeso=Euro*euro
+            print(f'La cantidad de {Euro} a Pesosr es de: {euroAPeso}')
+            os.system('pause')
+            os.system('cls')
+        elif opt=='4':
+            pesos=float(input('Ingrese la cantidad de pesos que desea convertir: '))
+            pesosAYenes=pesos/yen
+            print(f'La cantidad de {Euro} a Yenes es de: {pesosAYenes}')
+            os.system('pause')
+            os.system('cls')
+        else:
+            varBool=bool(input('Desea hacer otra conversion? s(si) enter(no)'))
+            os.system('cls')
 
-    pesos=float(input('Ingrese la cantidad de pesos que desea convertir: '))
-    Euro=float(input('Ingrese la cantidad de pesos que desea convertir: '))
-    pesosADolar= pesos/dolar
+            
 
-    pesosAEuro=pesos/euro
-
-    euroAPeso=Euro*euro
-
-    pesosAYenes=pesos/yen
 
 
 
